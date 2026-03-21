88bd243a0c91030c82e8483cdee61acbdb687103
jeffthebever2 <133807587+jeffthebever2@users.noreply.github.com>
2026-03-20 20:37:18 -0400

    Delete cloudflare/migrations directory

--- cloudflare/migrations/0002_thumb_columns.sql
+++
@@ -1,5 +0,0 @@
--- D1 Migration: 0002_thumb_columns.sql
--- Add dimensions and color info to photo metadata
-ALTER TABLE photo_metadata ADD COLUMN width_px   INTEGER;
-ALTER TABLE photo_metadata ADD COLUMN height_px  INTEGER;
-ALTER TABLE photo_metadata ADD COLUMN dominant_color TEXT;
